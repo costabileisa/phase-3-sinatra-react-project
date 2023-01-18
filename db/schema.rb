119b904068b308478473afe56374582f31a313e5
Isabella Costabile <costabileisa@gmail.com>
2023-01-18 13:18:46 -0500

    added name column to dogs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_183636) do
+ActiveRecord::Schema.define(version: 2023_01_18_172544) do
 
   create_table "breeds", force: :cascade do |t|
     t.string "breed"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_01_17_183636) do
     t.integer "breed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,13 @@ Breed.create(
 )
 Dog.create([
     {
+        name: "Marley",
         img_url: "https://www.lucypetproducts.com/wp-content/uploads/2020/01/Golden4.jpg",
         img_description: "adult golden retriever laying down outside", 
         breed_id: 1
     },
     {
+        name: "Jojo",
         img_url: "https://s36700.pcdn.co/wp-content/uploads/2019/11/Golden-Puppy_getty82781924-399x600.png",
         img_description: "golden retriever puppy laying down indoors",
         breed_id: 1
